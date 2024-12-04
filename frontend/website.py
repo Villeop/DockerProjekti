166b37785494a36b509145405c31a8b4c5795474
Villeop <villeop@gmail.com>
2024-12-04 17:54:44 +0200

    Tweaking the sites

--- frontend/website.py
+++ frontend/website.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/docker')
+def docker():
+    return render_template('docker.html')
+
+@app.route('/docker-compose')
+def docker_compose():
+    return render_template('docker-compose.html')
+
+@app.route('/notes')
+def notes():
+    return render_template('notes.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
